3d4f64c87718b5beb5f7cc22df615c4143293db3
Novikova Nadia <nadia30062004@mail.ru>
2024-07-06 16:20:04 +0300

    16:19

--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public GameObject beefPrefab;
+    public GameObject burgerPrefab;
+    public GameObject colaPrefab;
+    public GameObject fricePrefab;
     public float timeLeft;
     public Text timerText;
     public Canvas canvas;
@@ -32,13 +35,17 @@ public class GameController : MonoBehaviour
 
     IEnumerator Spawn()
     {
+        // Создание массива объектов
+        GameObject[] objects = new GameObject[]{burgerPrefab,beefPrefab, colaPrefab, fricePrefab };
+
+
         yield return new WaitForSeconds(2.0f);
         while (timeLeft>0)
         {
             Vector2 spawnPosition = new Vector2(
                 Random.Range(-canvasRect.rect.width / 2 + beefWidth / 2, canvasRect.rect.width / 2 - beefWidth / 2),
                 Random.Range(-canvasRect.rect.height, canvasRect.rect.height));
-            GameObject newBeef = Instantiate(beefPrefab, canvas.transform);
+            GameObject newBeef = Instantiate(objects[Random.Range(0, objects.Length)], canvas.transform);
             newBeef.GetComponent<RectTransform>().anchoredPosition = spawnPosition;
             yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
         }
